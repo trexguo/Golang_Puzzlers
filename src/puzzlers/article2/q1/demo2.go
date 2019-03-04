0695f57e6c9c2f5d939270da05fcf12e0625608c
trexguo <t.rex.guo@gmail.com>
2019-03-04 22:11:54 +0800

    comment

--- src/puzzlers/article2/q1/demo2.go
+++ src/puzzlers/article2/q1/demo2.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"flag"
+	"flag" // Go 语言标准库中有一个代码包专门用于接收和解析命令参数
 	"fmt"
 )
 
 var name string
 
+// go run demo2.go -name Rex
+// go run demo2.go --help
 func init() {
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
 }
